54a19c24dbae407d6e65605c7aa9c7caf87536a6
MoisesDelarosa <lalapkyla@gmail.com>
2024-09-12 12:46:16 +0800

    Adding Acads Page

---
+++ KylaGithubActivity/Pages/Acads/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KylaGithubActivity.Pages.Acads
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
